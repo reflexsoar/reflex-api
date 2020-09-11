5b2843b4ea1a0599c6cedaa19f1ca1b28796d451
Brian Carroll <bcarroll@zeroonesecurity.com>
2020-09-11 09:24:11 -0400

    removed catch all route, keep spa out of flask

--- app/__init__.py
+++ app/__init__.py
@@ -34,9 +34,4 @@ def create_app(environment='development'):
 
     FLASK_BCRYPT.init_app(app)
 
-    @app.route('/', defaults={'path': ''})
-    @app.route('/<path:path>')
-    def catch_all(path):
-        return app.send_static_file("index.html")
-
     return app
