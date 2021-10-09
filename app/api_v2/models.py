c5884f4232e938684af9a01d9051e2e2214418e2
Brian Carroll <bcarroll@zeroonesecurity.com>
2021-10-08 22:15:28 -0400

    additional SID enrichments

--- app/api_v2/models.py
+++ app/api_v2/models.py
@@ -621,21 +621,71 @@ class Observable(BaseDocument):
         '''
 
         # Well known SIDs for Microsoft Domains
-        ends_width = {
+        ends_with = {
+            '500': 'Administrator',
+            '501': 'Guest',
             '502': 'krbtgt',
-            '512': 'Domain Admin',
+            '512': 'Domain Admins',
             '513': 'Domain Users',
             '514': 'Domain Guests',
             '515': 'Domain Computers',
             '516': 'Domain Controllers',
             '517': 'Cert Publishers',
-            '518': 'Schema Admins'
+            '518': 'Schema Admins',
+            '519': 'Enterprise Admins',
+            '520': 'Group Policy Creator Owners',
+            '553': 'RAS AND IAS Servers',
+            '544': 'Administrators',
+            '545': 'Users',
+            '546': 'Guest',
+            '547': 'Power Users',
+            '548': 'Account Operators',
+            '549': 'Server Operators',
+            '550': 'Print Operators',
+            '551': 'Backup Operators',
+            '552': 'Replicators',
+            '554': 'Builtin\Pre-Windows 2000 Compatible Access',
+            '555': 'Builtin\Remote Desktop Users',
+            '556': 'Builtin\Network Configuration Operators',
+            '557': 'Builtin\Incoming Forest Trust Builders',
+            '558': 'Builtin\Performance Monitor Users',
+            '559': 'Builtin\Performance Log Users',
+            '560': 'Builtin\Windows Authorization Access Group',
+            '561': 'Builtin\Terminal Server License Servers',
+            '562': 'Builtin\Distributed COM Users',
+            '569': 'Builtin\Cryptographic Operators',
+            '573': 'Builtin\Event Log Readers'
+            '574': 'Builtin\Certificate Service DCOM Access',
+            '575': 'Builtin\RDS Remote Access Servers',
+            '576': 'Builtin\RDS Endpoint Servers',
+            '577': 'Builtin\RDS Management Servers',
+            '578': 'Builtin\Hyper-V Administrators',
+            '579': 'Builtin\Access Control Assistance Operators',
+            '580': 'Builtin\Remote Management Users'
+        }
+
+        equals = {
+            'S-1-5-64-10': 'NTLM Authentication',
+            'S-1-5-64-14': 'SChannel Authentication',
+            'S-1-5-64-21': 'Digest Authentication',
+            'S-1-16-0': 'Untrusted Mandatory Level',
+            'S-1-16-4096': 'Low Mandatory Level',
+            'S-1-16-8192': 'Medium Mandatory Level',
+            'S-1-16-8448': 'Medium Plus Mandatory Level',
+            'S-1-16-12288': 'High Mandatory Level',
+            'S-1-16-16384': 'System Mandatory Level',
+            'S-1-16-20480': 'Protected Process Mandatory Level',
+            'S-1-16-28672': 'Secure Process Mandatory Level'
         }
 
         if self.data_type == 'sid':
-            for k in ends_width:
+            for k in ends_with:
                 if self.value.endswith(k):
-                    self.add_tag(ends_width[k])
+                    self.add_tag(ends_with[k])
+
+            for k in equals:
+                if self.value == k:
+                    self.add_tag(equals[k])
 
         self.save()
 
