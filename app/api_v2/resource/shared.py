e9cbb3f6577a996c167fdaa61ea2b30114af56a4
Brian Carroll <bcarroll@zeroonesecurity.com>
2022-05-13 20:45:20 -0400

    BUG: Fixing user role permissions for detections

--- app/api_v2/resource/shared.py
+++ app/api_v2/resource/shared.py
@@ -206,8 +206,8 @@ mod_permissions = Model('Permissions', {
     'update_settings': fields.Boolean,
     'view_settings': fields.Boolean,
     'create_detection': fields.Boolean,
-    'update_detection:': fields.Boolean,
-    'view_detections:': fields.Boolean,
+    'update_detection': fields.Boolean,
+    'view_detections': fields.Boolean,
     'delete_detection': fields.Boolean,
     'create_persistent_pairing_token': fields.Boolean,
     'use_api': fields.Boolean(optional=True)
