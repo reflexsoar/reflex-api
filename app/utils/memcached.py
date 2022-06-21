381e0e7b484d293c6ddbf8652ff4648f4c8ce492
Brian Carroll <bcarroll@zeroonesecurity.com>
2022-06-21 12:55:59 -0400

    BUG: Globally available memcached client pool to avoid port exhaustion

--- app/utils/memcached.py
+++ app/utils/memcached.py
@@ -1,10 +1,20 @@
 from multiprocessing import Pool
 from pymemcache.client.base import PooledClient
 
+
 class MemcachedClient:
 
     def __init__(self, *args, **kwargs):
+        '''
+        Declares a MemcachedClient with no client set
+        '''
         self.client = None
 
     def init_app(self, app):
-        self.client = PooledClient(f"{app.config['THREAT_POLLER_MEMCACHED_HOST']}:{app.config['THREAT_POLLER_MEMCACHED_PORT']}", max_pool_size=4)
+        '''
+        Initializes the MemcachedClient with a Flask App
+        '''
+        
+        self.client = PooledClient(
+            f"{app.config['THREAT_POLLER_MEMCACHED_HOST']}:{app.config['THREAT_POLLER_MEMCACHED_PORT']}",
+            max_pool_size=app.config['MEMCACHED_POOL_SIZE'])
