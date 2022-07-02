30d16dff331138efeef5af705b2d29c268b37ea5
Brian Carroll <bcarroll@zeroonesecurity.com>
2022-07-01 20:27:06 -0400

    stuff

--- app/api_v2/resource/event.py
+++ app/api_v2/resource/event.py
@@ -1789,7 +1789,7 @@ class EventQueueStats(Resource):
         for worker in ep.workers:
             worker_info.append(
                 {
-                    '_id': worker._sentinel,
+                    'pid': worker.pid,
                     'alive': worker.is_alive()
                 }
             )
