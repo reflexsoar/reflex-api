6282604bded8f8854c43618fc511a8c129a2fcde
Brian Carroll <brian@hasecuritysolutions.com>
2023-02-23 20:55:13 -0500

    new observable metrics

--- app/api_v2/resource/observable.py
+++ app/api_v2/resource/observable.py
@@ -1,8 +1,14 @@
-from flask_restx import Model, fields
+from flask_restx import fields, Namespace, Resource
 from .shared import mod_pagination
+from ..utils import token_required, user_has
+from ..model import (
+    Event,
+    Q
+)
 
+api = Namespace('Observable', description="Observable operations", path="/observable")
 
-mod_observable_update = Model('ObservableUpdate', {
+mod_observable_update = api.model('ObservableUpdate', {
     'tags': fields.List(fields.String),
     'ioc': fields.Boolean,
     'tlp': fields.Integer,
@@ -11,7 +17,7 @@ mod_observable_update = Model('ObservableUpdate', {
     'data_type': fields.String
 })
 
-mod_observable_list = Model('ObservableList', {
+mod_observable_list = api.model('ObservableList', {
     'tags': fields.List(fields.String),
     'value': fields.String,
     'ioc': fields.Boolean,
@@ -25,13 +31,13 @@ mod_observable_list = Model('ObservableList', {
     'original_source_field': fields.String
 })
 
-mod_observable_list_paged = Model('PagedObservableList', {
+mod_observable_list_paged = api.model('PagedObservableList', {
     'observables': fields.List(fields.Nested(mod_observable_list)),
     'pagination': fields.Nested(mod_pagination)
 })
 
 
-mod_observable_create = Model('ObservableCreate', {
+mod_observable_create = api.model('ObservableCreate', {
     'value': fields.String(required=True),
     'ioc': fields.Boolean,
     'tlp': fields.Integer,
@@ -43,7 +49,44 @@ mod_observable_create = Model('ObservableCreate', {
     'original_source_field': fields.String
 })
 
-mod_bulk_add_observables = Model('BulkObservables', {
+mod_bulk_add_observables = api.model('BulkObservables', {
     'observables': fields.List(fields.Nested(mod_observable_create)),
     'organization': fields.String
 })
+
+mod_observable_event_hits = api.model('ObservableEventHits', {
+    'total_events': fields.Integer,
+    'organization_events': fields.Integer,
+    'total_cases': fields.Integer
+})
+
+
+
+@api.route('/<string:value>/hits')
+class ObservableHits(Resource):
+
+    @api.doc(security="Bearer")
+    @api.marshal_with(mod_observable_event_hits)
+    @token_required
+    @user_has('view_events')    
+    def get(self, value, current_user):
+        '''
+        Get observables that match a value
+        '''
+        search = Event().search()
+        search = search.query('nested', path='event_observables', query=Q('match', event_observables__value=value))
+        total_events = search.count()
+
+        search = Event().search()
+        search = search.filter('term', organization=current_user.organization)
+        search = search.query('nested', path='event_observables', query=Q('match', event_observables__value=value))
+        organization_events = search.count()
+
+        search = Event().search()
+        search.aggs.bucket('case_observable_metrics', 'filter', nested={
+            'path': 'event_observables', 'query': Q('match', event_observables__value=value)
+        }).bucket('cases', 'cardinality', field='case')
+        results = search.execute()
+        total_cases = results.aggregations.case_observable_metrics.cases.value
+
+        return {'total_events': total_events, 'organization_events': organization_events, "total_cases": total_cases}
