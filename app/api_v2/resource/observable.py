ff81812c1f2015616326c7b78b65612967c71a57
Brian Carroll <brian@hasecuritysolutions.com>
2023-02-25 00:25:14 -0500

    max hits use count instead

--- app/api_v2/resource/observable.py
+++ app/api_v2/resource/observable.py
@@ -108,10 +108,11 @@ class ObservableHits(Resource):
             search = search.filter('term', organization=current_user.organization)
         search = search.query('nested', path='event_observables', query=Q('term', event_observables__value__keyword=value))
 
+        organization_events = search.count()
+
         search.aggs.bucket('event_titles', 'terms', field='title', size=100)
         results = search.execute()
         event_titles = results.aggregations.event_titles.buckets
-        organization_events = results.hits.total.value
 
         top_events = [{'title': e.key, 'hits': e.doc_count} for e in event_titles]
 
