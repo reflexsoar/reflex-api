b2fc45614f620cc3db1aad853c5c503f6b6ead22
Brian Carroll <brian@hasecuritysolutions.com>
2023-11-06 20:55:39 -0500

    fim model start

---
+++ app/api_v2/model/fim.py
@@ -0,0 +1,37 @@
+
+
+
+    
+from . import (
+    base,
+    Keyword,
+    Text,
+    Boolean,
+    Integer
+)
+
+class FimRule(base.BaseDocument):
+
+    name = Keyword(fields={'text': Text()})
+    description = Keyword(fields={'text': Text()})
+    severity = Keyword()
+    risk_score = Integer()
+    paths = Keyword()
+    recursive = Boolean()
+    file_types = Keyword()
+    include_patterns = Keyword()
+    exclude_patterns = Keyword()
+    max_file_size = Integer()
+    max_parallel_files = Integer()
+    check_interval = Integer()
+    hashes = Keyword()
+    alert = Boolean()
+    collect_additional_data = Boolean()
+    tags = Keyword()
+
+    class Index:
+
+        name = 'reflex-fim-rules'
+        settings = {
+            'refresh_interval': '5s',
+        }
