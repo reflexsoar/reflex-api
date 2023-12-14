1a589a6b472ef09a5a0a397c92907beb48c671c5
Brian Carroll <brian@hasecuritysolutions.com>
2023-12-14 17:13:45 -0500

    search websockets

--- app/api_v2/resource/search.py
+++ app/api_v2/resource/search.py
@@ -2,11 +2,20 @@ import re
 import math
 import uuid
 from flask_restx import Resource, Namespace, fields
+from flask import render_template, make_response, request
+from flask_socketio import send, emit, join_room, leave_room
 
-from ..utils import token_required, user_has
+from ..utils import _check_token, token_required, user_has
 from .shared import ISO8601, mod_user_list
 
-from app.api_v2.model import Event, Case, EventRelatedObject, AgentLogMessage, Search
+from app.api_v2.model import (
+    Event, Case, EventRelatedObject, AgentLogMessage, Search,
+    SearchProxyJob
+)
+
+from app.api_v2.utils import _check_token
+
+from app import sock
 
 api = Namespace('Search', description='Search Workspace', path='/search')
 
@@ -36,13 +45,46 @@ mod_search_results = api.model('SearchResults', {
     'pages': fields.Integer(),
     'search_id': fields.String(),
     'details': fields.Raw(),
-    'timefield': fields.String()
+    'timefield': fields.String(),
+    'proxied': fields.Boolean(),
+    'search_channel': fields.String(),
+    'total_results': fields.Integer()
 })
 
+@api.route("/proxy_jobs/<uuid>/status")
+class ProxyJobStatus(Resource):
+
+    @api.doc(security='Bearer')
+    @token_required
+    def get(self, current_user, uuid):
+        ''' Returns the status of a single proxy job '''
+
+        job = SearchProxyJob.get_by_uuid(uuid)
+
+        if job is None:
+            api.abort(404, 'Job not found.')
+
+        return {
+            'status': job.status
+        }
+
+@api.route("/proxy_jobs")
+class ProxyJobList(Resource):
+
+    @api.doc(security='Bearer')
+    @token_required
+    def get(self, current_user):\
+    
+        jobs = SearchProxyJob.search().filter('term', complete=False).execute()
+
+        return {
+            'response': jobs.to_dict()
+        }
 
 @api.route("/query")
 class HuntingQuery(Resource):
 
+    @api.doc(security='Bearer')
     @token_required
     @api.expect(mod_search_query)
     @api.marshal_with(mod_search_results)
@@ -96,8 +138,30 @@ class HuntingQuery(Resource):
                 if expression.match('reflex'):
                     api.abort(400, 'Invalid dataset.')
 
+            proxy_job = SearchProxyJob(
+                job_details=api.payload,
+                status='pending',
+                complete=False
+            )
+            proxy_job.save(refresh='wait_for')
+
+            response = {
+                'response': [],
+                'pages': 0,
+                'details': {},
+                'timefield': date_field,
+                'proxied': True,
+                'search_id': proxy_job.uuid,
+                'search_channel': f"{proxy_job.uuid}:{proxy_job.organization}",
+                'total_results': 0
+            }
+
+            return response
+
             # Create a search against an index using the global connection
             search = Search(index=index)
+
+
         else:
             search = dataset_mapping[api.payload['dataset']].search()
 
@@ -191,13 +255,104 @@ class HuntingQuery(Resource):
 
             pages = math.ceil(results.hits.total.value / page_size)
 
+            proxy_job = SearchProxyJob(
+                job_details=api.payload,
+                status='complete',
+                complete=True
+            )
+            proxy_job.save(refresh='wait_for')
+
             response = {
                 'response': results.to_dict(),
                 'pages': pages,
-                # TODO: This will be used for the search proxy
-                'search_id': str(uuid.uuid4()),
+                'search_id': proxy_job.uuid,
+                'proxied': False,
+                'search_channel': f"{proxy_job.uuid}:{proxy_job.organization}",
                 'details': search_dict,
-                'timefield': date_field
+                'timefield': date_field,
+                'total_results': results.hits.total.value
             }
 
             return response
+
+"""
+WEBSOCKET HANDLERS
+"""
+
+def ws_token_required(f):
+    """
+    Checks the clients HTTP headers for an authorization token
+    and if it is valid then the current_user is set to the
+    user object for the token.  If the token is invalid then
+    the current_user is set to None.  Also attempts to
+    split the room name into the search_uuid and organization
+    """
+
+    def wrapper(*args, **kwargs):
+        
+        current_user = None
+        search_uuid = None
+        organization = None
+        room = None
+
+        try:
+            current_user = _check_token()
+            data = args[0]
+            if data and isinstance(data, dict):
+                if 'room' in data:
+                    room = data['room']
+                    search_uuid, organization = room.split(':')
+        except Exception as e:
+            pass
+
+        return f(*args, **kwargs, current_user=current_user, search_uuid=search_uuid, organization=organization, room=room)
+
+    wrapper.__doc__ = f.__doc__
+    wrapper.__name__ = f.__name__
+    return wrapper
+
+def check_user_permissions(user, organization):
+    """
+    Returns True if the user a member of the provided
+    organization or if the user is a default org user
+    """
+
+    if user:
+        if user.is_default_org():
+            return True
+    
+        if user.organization == organization:
+            return True
+    
+    return False
+
+    
+@sock.on('join')
+@ws_token_required
+def join(data, current_user, search_uuid, organization, room):
+    """ Joins a client to the room to exchange information.  The room
+    name is a combination of the search_uuid and the organization the
+    search belongs to search_uuid:organization_uuid
+    """
+
+    print(f"CURRENT USER: {current_user} | SEARCH UUID: {search_uuid} | ORGANIZATION: {organization} | ROOM: {room}")
+    print(f"CHECK USER PERMISSIONS: {check_user_permissions(current_user, organization)}")
+
+    
+    job = SearchProxyJob.get_by_uuid(search_uuid)
+
+    if job is None:
+        return False
+
+    join_room(room)
+
+    print(f"{current_user.uuid} has joined room {room}")
+
+    emit('message', {
+    'message': f'{current_user.uuid} has joined'}, to=room)
+    
+@sock.on('results')
+@ws_token_required
+def results(data, current_user, search_uuid, organization, room):
+
+    emit('results', data['results'], to=room, broadcast=True)
