65ea333e97c23cb5c5b2416f7185f783df3875c5
Brian Carroll <brian@hasecuritysolutions.com>
2024-01-05 23:44:39 -0500

    application signatures and test update code

--- app/api_v2/model/application.py
+++ app/api_v2/model/application.py
@@ -1,7 +1,10 @@
+from hashlib import sha1 
+
 from . import (
     Keyword,
     base,
     Object,
+    Boolean
 )
 
 class ApplicationInventory(base.BaseDocument):
@@ -24,6 +27,8 @@ class ApplicationInventory(base.BaseDocument):
     package_name = Keyword()  # The package name of the software package
     url_info_about = Keyword()  # The URL info about of the software package
     language = Keyword()  # The language of the software package
+    application_signature = Keyword()  # The hash of the software package, which is a combination of the name, version, and vendor
+    deleted = Boolean()
     
     class Index:
         name = 'reflex-application-inventory'
@@ -35,6 +40,66 @@ class ApplicationInventory(base.BaseDocument):
     def bulk_add(cls, agent, applications):
         '''
         Bulk adds application inventory data
+        '''
+
+        '''
+        try:
+            # Find all the existing applications for this agent
+            existing_applications = cls.search().filter('term', agent__uuid=agent.uuid).scan()
+
+            # Update any existing applications that have changed
+            bulk_updates = []
+            for application in applications:
+                _existing_application = next((x for x in existing_applications if x.identifying_number == application['identifying_number']), None)
+                if _existing_application:
+                    if cls.compute_signature(application['name'], application['version'], application['vendor']) != _existing_application.application_signature:
+                        bulk_updates.append({
+                            '_op_type': 'update',
+                            '_index': cls.Index.name,
+                            '_type': cls._doc_type.name,
+                            '_id': _existing_application.meta.id,
+                            'doc': application
+                        })
+
+            # Update existing applications that no longer exist with deleted=True
+            for application in existing_applications:
+                if application.identifying_number not in [x['identifying_number'] for x in applications]:
+                    bulk_updates.append({
+                        '_op_type': 'update',
+                        '_index': cls.Index.name,
+                        '_type': cls._doc_type.name,
+                        '_id': application.meta.id,
+                        'doc': {
+                            'deleted': True
+                        }
+                    })
+
+            # Bulk update the applications
+            if len(bulk_updates) > 0:
+                cls.bulk(bulk_updates)
+        except Exception:
+            return
+
+        # For new applications, add them one at a time
+        for application in applications:
+            _existing_application = next((x for x in existing_applications if x.identifying_number == application['identifying_number']), None)
+            if not _existing_application:
+                cls(
+                    agent={'name': agent.name, 'uuid': agent.uuid},
+                    name=application['name'],
+                    version=application['version'],
+                    vendor=application['vendor'],
+                    identifying_number=application['identifying_number'],
+                    install_date=application['install_date'],
+                    install_source=application['install_source'],
+                    local_package=application['local_package'],
+                    package_cache=application['package_cache'],
+                    package_code=application['package_code'],
+                    package_name=application['package_name'],
+                    url_info_about=application['url_info_about'],
+                    language=application['language'],
+                    deleted=False
+                ).save()
         '''
 
         # Remove all existing application inventory data for this agent
@@ -50,3 +115,45 @@ class ApplicationInventory(base.BaseDocument):
             }
             application['organization'] = agent.organization
             cls(**application).save()
+
+    @classmethod
+    def compute_signature(cls, name, version, vendor):
+        '''
+        Computes the application signature
+        '''
+
+        _signature = f"{name}{version}{vendor}"
+
+        return sha1(_signature.encode('utf-8')).hexdigest()
+
+    def save(self, **kwargs):
+        '''
+        Save the document
+        '''
+
+        _signature = f"{self.name}{self.version}{self.vendor}"
+
+        self.application_signature = sha1(_signature.encode('utf-8')).hexdigest()
+
+        # pylint: disable=arguments-differ
+        return super().save(**kwargs)
+    
+    def update(self, **kwargs):
+        '''
+        Update the document
+        '''
+
+        if 'name' in kwargs:
+            self.name = kwargs['name']
+        if 'version' in kwargs:
+            self.version = kwargs['version']
+        if 'vendor' in kwargs:
+            self.vendor = kwargs['vendor']
+
+        _signature = f"{self.name}{self.version}{self.vendor}"
+
+        self.application_signature = sha1(_signature.encode('utf-8')).hexdigest()
+
+        # pylint: disable=arguments-differ
+        return super().update(**kwargs)
+
